47297d76143423fe6222f0c74d326b82b187a766
Elena <33256869+Ledo1988@users.noreply.github.com>
2020-07-12 22:01:26 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -10,18 +10,59 @@ sendbtn.addEventListener('click', function() {
 //Переключаем блоки формы
 function switchForm() {
 	let index = 0;
+	let rateChange = 0;
+	let iceRate = 0;
+	let rateReduction = 0;
 	
 	let prevButton = document.getElementsByClassName('main-form__btn-prev')[0];
     let nextButton = document.getElementsByClassName('main-form__btn-next')[0];
 	prevButton.addEventListener('click', function() { go(this); }, false);
     nextButton.addEventListener('click', function() { go(this); }, false);
 	
+	let iceAdd = document.querySelectorAll('.radio-inline__input-ice');
+	iceAdd.forEach(item => {item.addEventListener('click', () => { iceRateCalc(item) }) });
+	
+	let rateReduce = document.querySelectorAll('.radio-inline__checkbox-percent');
+	rateReduce.forEach(item => {item.addEventListener('click', () => { rateReduceCalc(item) }) });
+
+	
+	function rateReduceCalc(el) {
+
+		let totalPercent = 0;		
+	
+		for (let i = 0; i < rateReduce.length; i++) {
+			if (rateReduce[i].checked) {
+				totalPercent =  totalPercent + Number.parseInt(rateReduce[i].dataset.type);
+			}
+        } 
+		
+		if (iceRate > 0) {
+			rateReduction = Math.round(4860*totalPercent/100/5)*5;
+		} else {
+			rateReduction = Math.round(3975*totalPercent/100/5)*5;
+		}
+	}
+	
+	
+	
+	function iceRateCalc(el) {	
+	
+		if (el && el.checked && el.dataset.type == 'positive') {
+			iceRate = 885;	
+		} else {
+			iceRate = 0;
+		}
+		
+		rateReduceCalc();
+	}
+	
+	
 	function go(el) {
 		let finalRate = 3785;
 		let buttons = document.querySelectorAll(".main-form__btn");
 		let items = document.querySelectorAll(".main-form .form-group");
 		let progressBar = document.getElementsByClassName('rate-bar__done')[0];
-		let progressRate = document.getElementsByClassName('rate-bar__rate-calc')[0];
+		let progressRate = document.getElementsByClassName('rate-bar__rate-calc')[0];		
 		
 		for(let i = 0; i < buttons.length; i++) {			 
 			  buttons[i].classList.remove('main-form__btn_disable');
@@ -35,6 +76,8 @@ function switchForm() {
 			if (index == items.length - 1) {
 				el.className += " main-form__btn_disable";
 			} 
+				
+			
 		}
 		
 		if (el.dataset.type == 'prev') {			
@@ -43,23 +86,62 @@ function switchForm() {
 			
 			if (index == 0) {
 				el.className += " main-form__btn_disable";
-			} 
+			} 		
+			
         }
-		progressRate.textContent = Math.round(finalRate*index/(items.length - 2));
-		
+				
 		progressBar.style.width = index/(items.length - 1)*100 + '%';
 		
 		for (let i = 0; i < items.length; i++) {
+			
             items[i].classList.remove("form-group_active");	
-
-            if (i == index)
-              items[i].className += " form-group_active";
-			  
+			
+			if (i == index)
+				items[i].className += " form-group_active";
 			
         } 
-
+		
+			
+		if (index == 0) {					
+			rateChange = (el.dataset.type == 'next') ? 0: rateChange - 150;
+		}		
+		if (index == 1) {					
+			rateChange = (el.dataset.type == 'next') ? rateChange + 150: rateChange - 855;
+		}
+		if (index == 2) {					
+			rateChange = (el.dataset.type == 'next') ? rateChange + 855: rateChange - 1800;
+		}
+		if (index == 3) {					
+			rateChange = (el.dataset.type == 'next') ? rateChange + 1800: rateChange - 530;
+		}
+		if (index == 4) {					
+			rateChange = (el.dataset.type == 'next') ? rateChange + 530: rateChange - 555;
+		}
+		if (index == 5)	{
+			rateChange = (el.dataset.type == 'next') ? rateChange + 555: rateChange - iceRate;
+		}
+		
+		if (index == 6)	{
+			rateChange = (el.dataset.type == 'next') ? rateChange + iceRate: rateChange - 85;
+		}
+				
+		if (index == 7) {					
+			rateChange = (el.dataset.type == 'next') ? rateChange + 85: rateChange + rateReduction;
+		}
+		
+		if (index == 8) {					
+			rateChange = (el.dataset.type == 'next') ? rateChange - rateReduction: rateChange;
+		}
+		
+	
+		
+		progressRate.textContent = rateChange;
 				
 	}
+	
+	
+	
+	
 }
 
 switchForm();
